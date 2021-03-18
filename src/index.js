67a0ba45d1c49c6f09f71045a151d18cd304cc29
Miguel Ricaño <mricanho@gmail.com>
2021-03-18 11:20:42 -0600

    Create Event listener and Create list function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
-const listContainer = document.getElementById('data-lists');
-const newListForm = document.getElementById('data-new-list-form');
-const newListInput= document.getElementById('data-new-list-input');
+const listContainer = document.getElementById('data-lists')
+const newListForm = document.getElementById('data-new-list-form')
+const newListInput= document.getElementById('data-new-list-input')
 
-let lists = [{id: 1, name: 'name'}, {id: 2, name: 'name'}];
+let lists = [{
+  id: 1, name: 'name'
+}, {
+  id: 2, name: 'name'
+}]
+
+newListForm.addEventListener('submit', e => {
+  e.preventDefault()
+  const listName = newListInput.value
+  if (listName == null || listName === '') return
+  const list = createList(listName)
+  newListInput.value = null
+  lists.push(list)
+  render()
+})
+
+function createList(name) {
+  return { id: Date.now().toString(), name: name, tasks: [] }
+}
 
 function render() {
   clearElement(listContainer);

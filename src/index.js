ed6b255165aff7d70a6457b3b8f3ffa3f14c67f6
BrianSammit <briansammit.cruz@gmail.com>
2021-03-18 12:37:42 -0500

    Add save to local storage and saveAndRender funtion

--- src/index.js
+++ src/index.js
@@ -1,41 +1,47 @@
-const listContainer = document.getElementById('data-lists')
-const newListForm = document.getElementById('data-new-list-form')
-const newListInput= document.getElementById('data-new-list-input')
+const listContainer = document.getElementById("data-lists");
+const newListForm = document.getElementById("data-new-list-form");
+const newListInput = document.getElementById("data-new-list-input");
 
-let lists = [{
-  id: 1, name: 'name'
-}, {
-  id: 2, name: 'name'
-}]
+const LOCAL_STORAGE_LIST_KEY = "task.lists";
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
 
-newListForm.addEventListener('submit', e => {
-  e.preventDefault()
-  const listName = newListInput.value
-  if (listName == null || listName === '') return
-  const list = createList(listName)
-  newListInput.value = null
-  lists.push(list)
-  render()
-})
+newListForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const listName = newListInput.value;
+  if (listName == null || listName === "") return;
+  const list = createList(listName);
+  newListInput.value = null;
+  lists.push(list);
+  saveAndRender();
+});
 
 function createList(name) {
-  return { id: Date.now().toString(), name: name, tasks: [] }
+  return { id: Date.now().toString(), name: name, tasks: [] };
+}
+
+function saveAndRender() {
+  save();
+  render();
+}
+
+function save() {
+  localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
 }
 
 function render() {
   clearElement(listContainer);
-  lists.forEach(list => {
-    const listElement = document.createElement('li')
-    listElement.dataset.listId = list.id
-    listElement.classList.add("subtitle")
-    listElement.innerText = list.name
-    listContainer.appendChild(listElement)
-  })
+  lists.forEach((list) => {
+    const listElement = document.createElement("li");
+    listElement.dataset.listId = list.id;
+    listElement.classList.add("subtitle");
+    listElement.innerText = list.name;
+    listContainer.appendChild(listElement);
+  });
 }
 
 function clearElement(element) {
   while (element.firstChild) {
-    element.removeChild(element.firstChild)
+    element.removeChild(element.firstChild);
   }
 }
 

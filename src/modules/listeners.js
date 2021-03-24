b04ac1003a75feb765b53005943bcecbb308de6f
BrianSammit <briansammit.cruz@gmail.com>
2021-03-24 14:57:19 -0500

    Refactor functions to arrow functions

--- src/modules/listeners.js
+++ src/modules/listeners.js
@@ -6,46 +6,45 @@ import {
   formLogic,
   submitList,
   clickHandler,
-} from './logic';
+} from "./logic";
 
-const displayModal = document.getElementById('launch-modal');
-const closeModal = document.querySelector('#close-modal');
-const openTaskForm = document.getElementById('newtask-b');
-const deleteListButton = document.getElementById('data-delete-list-button');
-const cancelForm = document.getElementById('cancel-form');
-const newListForm = document.getElementById('data-new-list-form');
+const displayModal = document.getElementById("launch-modal");
+const closeModal = document.querySelector("#close-modal");
+const openTaskForm = document.getElementById("newtask-b");
+const deleteListButton = document.getElementById("data-delete-list-button");
+const cancelForm = document.getElementById("cancel-form");
+const newListForm = document.getElementById("data-new-list-form");
 
-
-function listeners() {
-  openTaskForm.addEventListener('click', () => {
-    displayModal.classList.add('is-active');
+const listeners = () => {
+  openTaskForm.addEventListener("click", () => {
+    displayModal.classList.add("is-active");
   });
 
-  closeModal.addEventListener('click', () => {
-    displayModal.classList.remove('is-active');
+  closeModal.addEventListener("click", () => {
+    displayModal.classList.remove("is-active");
   });
 
-  cancelForm.addEventListener('click', () => {
-    displayModal.classList.remove('is-active');
+  cancelForm.addEventListener("click", () => {
+    displayModal.classList.remove("is-active");
   });
 
-  document.addEventListener('keydown', ({ key }) => {
-    if (key === 'Escape') {
-      displayModal.classList.remove('is-active');
+  document.addEventListener("keydown", ({ key }) => {
+    if (key === "Escape") {
+      displayModal.classList.remove("is-active");
     }
   });
 
-  deleteListButton.addEventListener('click', deleteProject);
+  deleteListButton.addEventListener("click", deleteProject);
 
-  listContainer.addEventListener('click', selectListContainer);
+  listContainer.addEventListener("click", selectListContainer);
 
-  taskForm.addEventListener('click', formLogic);
+  taskForm.addEventListener("click", formLogic);
 
-  taskForm.addEventListener('submit', formLogic);
+  taskForm.addEventListener("submit", formLogic);
 
-  newListForm.addEventListener('submit', submitList);
+  newListForm.addEventListener("submit", submitList);
 
-  document.addEventListener('click', clickHandler);
-}
+  document.addEventListener("click", clickHandler);
+};
 
 export { listeners };
--- src/modules/logic.js
+++ src/modules/logic.js
@@ -1,78 +1,78 @@
-const listContainer = document.getElementById('data-lists');
-const newListInput = document.getElementById('data-new-list-input');
+const listContainer = document.getElementById("data-lists");
+const newListInput = document.getElementById("data-new-list-input");
 const listDisplayContainer = document.getElementById(
-  'data-list-display-container',
+  "data-list-display-container"
 );
-const listTitleElemnt = document.getElementById('data-list-title');
-const tasksContainer = document.getElementById('data-tasks');
-const taskTemplate = document.getElementById('task-template');
-const displayModal = document.getElementById('launch-modal');
-const taskForm = document.querySelector('[data-submit-task]');
-const titleTask = document.querySelector('[data-title-task]');
-const descriptionTask = document.querySelector('[data-description-task]');
-const dateTask = document.querySelector('[data-date-task]');
-const priorityTask = document.querySelector('[data-priority-task]');
+const listTitleElemnt = document.getElementById("data-list-title");
+const tasksContainer = document.getElementById("data-tasks");
+const taskTemplate = document.getElementById("task-template");
+const displayModal = document.getElementById("launch-modal");
+const taskForm = document.querySelector("[data-submit-task]");
+const titleTask = document.querySelector("[data-title-task]");
+const descriptionTask = document.querySelector("[data-description-task]");
+const dateTask = document.querySelector("[data-date-task]");
+const priorityTask = document.querySelector("[data-priority-task]");
 
-const LOCAL_STORAGE_LIST_KEY = 'task.lists';
-const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId';
+const LOCAL_STORAGE_LIST_KEY = "task.lists";
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = "task.selectedListId";
 let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
 let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY);
 
-function deleteProject(e) {
+const deleteProject = (e) => {
   lists = lists.filter((list) => list.id !== selectedListId);
   selectedListId = null;
   saveAndRender();
-}
+};
 
-function selectListContainer(e) {
-  if (e.target.tagName.toLowerCase() === 'li') {
+const selectListContainer = (e) => {
+  if (e.target.tagName.toLowerCase() === "li") {
     selectedListId = e.target.dataset.listId;
     saveAndRender();
   }
-}
+};
 
-function formLogic(e) {
+const formLogic = (e) => {
   e.preventDefault();
   const taskTitle = titleTask.value;
-  if (taskTitle == null || taskTitle === '') return;
+  if (taskTitle == null || taskTitle === "") return;
   const taskDescription = descriptionTask.value;
-  if (taskDescription == null || taskDescription === '') return;
+  if (taskDescription == null || taskDescription === "") return;
   const taskDate = dateTask.value;
 
-  if (taskDate == null || taskDate === '') return;
+  if (taskDate == null || taskDate === "") return;
   const taskPriority = priorityTask.value;
-  if (taskPriority == null || taskPriority === '') return;
+  if (taskPriority == null || taskPriority === "") return;
   const task = createTask(taskTitle, taskDescription, taskDate, taskPriority);
   titleTask.value = null;
   descriptionTask.value = null;
   dateTask.value = null;
   priorityTask.value = null;
-  displayModal.classList.remove('is-active');
+  displayModal.classList.remove("is-active");
 
   const selectedList = lists.find((list) => list.id === selectedListId);
   selectedList.tasks.push(task);
   saveAndRender();
-}
+};
 
-function submitList(e) {
+const submitList = (e) => {
   e.preventDefault();
   const listName = newListInput.value;
-  if (listName == null || listName === '') return;
+  if (listName == null || listName === "") return;
   const list = createList(listName);
   newListInput.value = null;
   lists.push(list);
   saveAndRender();
-}
+};
 
-function createList(name) {
+const createList = (name) => {
   return {
     id: Date.now().toString(),
     name,
     tasks: [],
   };
-}
+};
 
-function createTask(name, description, date, priority) {
+const createTask = (name, description, date, priority) => {
   return {
     id: Date.now().toString(),
     name,
@@ -80,52 +80,52 @@ function createTask(name, description, date, priority) {
     date,
     priority,
   };
-}
+};
 
-function saveAndRender() {
+const saveAndRender = () => {
   save();
   render();
-}
+};
 
-function save() {
+const save = () => {
   localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
   localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId);
-}
+};
 
-function render() {
+const render = () => {
   clearElement(listContainer);
   renderLists();
   showRender();
-}
+};
 
-function showRender() {
+const showRender = () => {
   const selectedList = lists.find((list) => list.id === selectedListId);
   if (selectedListId == null) {
-    listDisplayContainer.style.display = 'none';
+    listDisplayContainer.style.display = "none";
   } else {
-    listDisplayContainer.style.display = '';
+    listDisplayContainer.style.display = "";
     listTitleElemnt.innerText = selectedList.name;
     clearElement(tasksContainer);
     renderTasks(selectedList);
   }
-}
+};
 
-function renderTasks(selectedList) {
+const renderTasks = (selectedList) => {
   selectedList.tasks.forEach((task) => {
     taskManipulation(task);
   });
-}
+};
 
-function taskManipulation(task) {
+const taskManipulation = (task) => {
   const taskElement = document.importNode(taskTemplate.content, true);
-  const listElement = document.createElement('li');
+  const listElement = document.createElement("li");
   listElement.dataset.listId = task.id;
-  const checkbox = taskElement.querySelector('input');
+  const checkbox = taskElement.querySelector("input");
   checkbox.id = task.id;
-  const contentTask = taskElement.querySelector('p');
-  const descTask = taskElement.getElementById('description');
-  const dateTask = taskElement.getElementById('date-task');
-  const priorityTask = taskElement.getElementById('final-priority');
+  const contentTask = taskElement.querySelector("p");
+  const descTask = taskElement.getElementById("description");
+  const dateTask = taskElement.getElementById("date-task");
+  const priorityTask = taskElement.getElementById("final-priority");
 
   contentTask.htmlfor = task.id;
   contentTask.append(task.name);
@@ -135,35 +135,35 @@ function taskManipulation(task) {
   priorityTask.append(task.priority);
   listElement.appendChild(taskElement);
   tasksContainer.appendChild(listElement);
-}
+};
 
-function renderLists() {
+const renderLists = () => {
   lists.forEach((list) => {
-    const listElement = document.createElement('li');
+    const listElement = document.createElement("li");
     listElement.dataset.listId = list.id;
     listElement.setAttribute(
-      'class',
-      'subtitle ml-3 is-justify-content-space-between',
+      "class",
+      "subtitle ml-3 is-justify-content-space-between"
     );
     listElement.innerText = list.name;
     if (list.id === selectedListId) {
-      listElement.classList.add('active-list');
+      listElement.classList.add("active-list");
     }
     listContainer.appendChild(listElement);
   });
-}
+};
 
-function clearElement(element) {
+const clearElement = (element) => {
   while (element.firstChild) {
     element.removeChild(element.firstChild);
   }
-}
+};
 
-function clickHandler(e) {
-  if (e.target.matches('.tryYes')) {
-    alert('Please');
+const clickHandler = (e) => {
+  if (e.target.matches(".tryYes")) {
+    alert("Please");
   }
-}
+};
 
 export {
   render,

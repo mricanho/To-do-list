fdea60297a173c80c4b37b9f6e9de6294ec11888
BrianSammit <briansammit.cruz@gmail.com>
2021-03-25 17:25:33 -0500

    Fix linters errors

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# To-do-list
-A simple task manager for projects. 
+# Todo-List
+
+This project consists in build a Todo-List. Using the concepts of Single Responsibility and Tightly Coupled Objects. In this project, you can create a new Project and inside this project, you can create a task, the task contains a title, description, due date, and priority.
+
+![screenshot](dist/assets/images/screen-1.PNG)
+
+## Live demo
+
+[live demo]()
+
+## Built With
+
+- JavaScript
+- HTML
+- CSS
+- Webpack
+- Package.json
+
+## How to start the project from your Local enviroment
+
+- Open your terminal and cd where you want to store the project
+- Run the following command - `git clone https://github.com/mricanho/To-do-list.git`
+- Cd into the directory - `cd To-do-list$`
+- Open the live server on your VScode `Alt + L` then `Alt + o` on `idex.html`
+
+## Author
+
+👤 **Brian Cruz**
+
+- Github: [@githubhandle](https://github.com/BrianSammit)
+- Twitter: [@twitterhandle](https://twitter.com/cruzsammit)
+- Linkedin: [linkedin](https://www.linkedin.com/in/brian-sammit-cruz-rodriguez-5877551a8/)
+
+👤 **Miguel Ricaño**
+
+- Github: [@mricanho](https://github.com/mricanho)
+- Linkedin: [Miguel Ricaño](https://www.linkedin.com/in/mricanho/)
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!
+
+## Show your support
+
+Give a star if you :star: like this project!
+
+## Acknowledgments
+
+- Microverse
+- GitHub
+- TheOdinProject
+- JavaScript
--- src/modules/logic.js
+++ src/modules/logic.js
@@ -1,24 +1,24 @@
-const listContainer = document.getElementById('data-lists');
-const newListInput = document.getElementById('data-new-list-input');
+const listContainer = document.getElementById("data-lists");
+const newListInput = document.getElementById("data-new-list-input");
 const listDisplayContainer = document.getElementById(
-  'data-list-display-container',
+  "data-list-display-container"
 );
-const listTitleElemnt = document.getElementById('data-list-title');
-const tasksContainer = document.getElementById('data-tasks');
-const taskTemplate = document.getElementById('task-template');
-const displayModal = document.getElementById('launch-modal');
-const taskForm = document.querySelector('[data-submit-task]');
-const titleTask = document.querySelector('[data-title-task]');
-const descriptionTask = document.querySelector('[data-description-task]');
-const dateTask = document.querySelector('[data-date-task]');
-const priorityTask = document.querySelector('[data-priority-task]');
+const listTitleElemnt = document.getElementById("data-list-title");
+const tasksContainer = document.getElementById("data-tasks");
+const taskTemplate = document.getElementById("task-template");
+const displayModal = document.getElementById("launch-modal");
+const taskForm = document.querySelector("[data-submit-task]");
+const titleTask = document.querySelector("[data-title-task]");
+const descriptionTask = document.querySelector("[data-description-task]");
+const dateTask = document.querySelector("[data-date-task]");
+const priorityTask = document.querySelector("[data-priority-task]");
 
-const LOCAL_STORAGE_LIST_KEY = 'task.lists';
-const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId';
+const LOCAL_STORAGE_LIST_KEY = "task.lists";
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = "task.selectedListId";
 let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
 let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY);
 
-const taskModal = document.getElementById('task-modal');
+const taskModal = document.getElementById("task-modal");
 
 const deleteProject = (e) => {
   lists = lists.filter((list) => list.id !== selectedListId);
@@ -27,7 +27,7 @@ const deleteProject = (e) => {
 };
 
 const selectListContainer = (e) => {
-  if (e.target.tagName.toLowerCase() === 'li') {
+  if (e.target.tagName.toLowerCase() === "li") {
     selectedListId = e.target.dataset.listId;
     saveAndRender();
   }
@@ -36,19 +36,19 @@ const selectListContainer = (e) => {
 const formLogic = (e) => {
   e.preventDefault();
   const taskTitle = titleTask.value;
-  if (taskTitle == null || taskTitle === '') return;
+  if (taskTitle == null || taskTitle === "") return;
   const taskDescription = descriptionTask.value;
-  if (taskDescription == null || taskDescription === '') return;
+  if (taskDescription == null || taskDescription === "") return;
   const taskDate = dateTask.value;
-  if (taskDate == null || taskDate === '') return;
+  if (taskDate == null || taskDate === "") return;
   const taskPriority = priorityTask.value;
-  if (taskPriority == null || taskPriority === '') return;
+  if (taskPriority == null || taskPriority === "") return;
   const task = createTask(taskTitle, taskDescription, taskDate, taskPriority);
   titleTask.value = null;
   descriptionTask.value = null;
   dateTask.value = null;
   priorityTask.value = null;
-  displayModal.classList.remove('is-active');
+  displayModal.classList.remove("is-active");
   const selectedList = lists.find((list) => list.id === selectedListId);
   selectedList.tasks.push(task);
   saveAndRender();
@@ -57,7 +57,7 @@ const formLogic = (e) => {
 const submitList = (e) => {
   e.preventDefault();
   const listName = newListInput.value;
-  if (listName == null || listName === '') return;
+  if (listName == null || listName === "") return;
   const list = createList(listName);
   newListInput.value = null;
   lists.push(list);
@@ -65,7 +65,7 @@ const submitList = (e) => {
 };
 
 const defaultProject = () => {
-  const list = createList('Default Project');
+  const list = createList("Default Project");
   if (lists === undefined || lists.length == 0) {
     lists.push(list);
   }
@@ -74,10 +74,10 @@ const defaultProject = () => {
 
 const defaultTask = () => {
   const task = createTask(
-    'The firt task',
-    'Here you need to write the description of the task',
-    '2021-12-12',
-    '9',
+    "The firt task",
+    "Here you need to write the description of the task",
+    "2021-12-12",
+    "9"
   );
   const selectedList = lists[0];
   if (selectedList.tasks === undefined || selectedList.tasks.length == 0) {
@@ -119,9 +119,9 @@ const render = () => {
 const showRender = () => {
   const selectedList = lists.find((list) => list.id === selectedListId);
   if (selectedListId == null) {
-    listDisplayContainer.style.display = 'none';
+    listDisplayContainer.style.display = "none";
   } else {
-    listDisplayContainer.style.display = '';
+    listDisplayContainer.style.display = "";
     listTitleElemnt.innerText = selectedList.name;
     clearElement(tasksContainer);
     renderTasks(selectedList);
@@ -136,17 +136,14 @@ const renderTasks = (selectedList) => {
 
 const taskManipulation = (task) => {
   const taskElement = document.importNode(taskTemplate.content, true);
-  const listElement = document.createElement('li');
+  const listElement = document.createElement("li");
   listElement.dataset.listId = task.id;
-  const checkbox = taskElement.querySelector('input');
+  const checkbox = taskElement.querySelector("input");
   checkbox.id = task.id;
-  const contentTask = taskElement.querySelector('p');
-  const descTask = taskElement.getElementById('description');
-  const dateTask = taskElement.getElementById('date-task');
-  const priorityTask = taskElement.getElementById('final-priority');
-  const buttonEdit = taskElement.getElementById('edit');
-
-  buttonEdit.setAttribute('id', task.id);
+  const contentTask = taskElement.querySelector("p");
+  const descTask = taskElement.getElementById("description");
+  const dateTask = taskElement.getElementById("date-task");
+  const priorityTask = taskElement.getElementById("final-priority");
 
   contentTask.htmlfor = task.id;
   contentTask.append(task.name);
@@ -160,15 +157,15 @@ const taskManipulation = (task) => {
 
 const renderLists = () => {
   lists.forEach((list) => {
-    const listElement = document.createElement('li');
+    const listElement = document.createElement("li");
     listElement.dataset.listId = list.id;
     listElement.setAttribute(
-      'class',
-      'subtitle ml-3 is-justify-content-space-between',
+      "class",
+      "subtitle ml-3 is-justify-content-space-between"
     );
     listElement.innerText = list.name;
     if (list.id === selectedListId) {
-      listElement.classList.add('active-list');
+      listElement.classList.add("active-list");
     }
     listContainer.appendChild(listElement);
   });
@@ -181,19 +178,19 @@ const clearElement = (element) => {
 };
 
 const clickHandler = (e) => {
-  if (e.target.matches('.tryYes')) {
-    taskModal.classList.add('is-active');
+  if (e.target.matches(".tryYes")) {
+    taskModal.classList.add("is-active");
   }
 };
 
 const editTask = () => {
-  const button = [...document.querySelectorAll('.tryYes')];
+  const button = [...document.querySelectorAll(".tryYes")];
   button.map((btn) => {
-    btn.addEventListener('click', (e) => {
+    btn.addEventListener("click", (e) => {
       const { id } = e.target;
       const projectIndex = getProjectIndex(selectedListId);
       const taskIndex = lists[projectIndex].tasks.findIndex(
-        (task) => task.id == id,
+        (task) => task.id == id
       );
 
       lists[projectIndex][taskIndex] = newValue;

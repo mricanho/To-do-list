76cce2adab1f641b7087578d1aa50e4f6855f00d
BrianSammit <briansammit.cruz@gmail.com>
2021-04-16 13:26:13 -0500

    Add test to logic

--- src/test/logic.test.js
+++ src/test/logic.test.js
@@ -1,4 +1,40 @@
-it("should work", () => {
-  const thisIsTrue = true;
-  expect(thisIsTrue).toBe(true);
+import jsdom from "jsdom";
+const { JSDOM } = jsdom;
+
+describe("MODULE RETRIEVE PROJECTS TEST", () => {
+  beforeEach(() =>
+    JSDOM.fromFile("./dist/index.html").then((dom) => {
+      document.body.innerHTML = dom.window.document.body.outerHTML;
+    })
+  );
+
+  it("Expects the projects projects container to not be null", () => {
+    const container = document.getElementsByClassName("g-container");
+    expect(container).toBeDefined();
+  });
+
+  it("Expects the projects section to not be null", () => {
+    const section = document.getElementsByClassName("section");
+    expect(section).toBeDefined();
+  });
+
+  it("Expects the projects modal to not be null", () => {
+    const modal = document.getElementById("launch-modal");
+    expect(modal).toBeDefined();
+  });
+
+  it("Expects the projects modal to not be null", () => {
+    const modal = document.getElementById("task-modal");
+    expect(modal).toBeDefined();
+  });
+
+  it("Expects the projects modal to not be null", () => {
+    const modal = document.getElementById("delete-modal");
+    expect(modal).toBeDefined();
+  });
+
+  it("Expects the projects task container to not be null", () => {
+    const container = document.getElementsByClassName("task-container");
+    expect(container).toBeDefined();
+  });
 });

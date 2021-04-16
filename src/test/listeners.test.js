b36303930b71b123b2ef4b38cc5a4c45790cc333
Miguel Ricaño <mricanho@gmail.com>
2021-04-16 15:17:49 -0500

    Set up babel

--- src/test/listeners.test.js
+++ src/test/listeners.test.js
@@ -1,1 +1,24 @@
-import listeners from '../modules/listeners';
+describe('My Test', () => {
+	let sut;
+	let events = {};
+
+	beforeEach(() => {
+		sut = new Dependency();
+
+		// Empty our events before each test case
+		events = {};
+
+		// Define the addEventListener method with a Jest mock function
+		document.addEventListener = jest.fn((event, callback) => {
+      		events[event] = callback;
+    	});
+	});
+
+	test('Test Keypress fires callback', () => {
+		// Watch the keypress callback
+		jest.spyOn(sut, 'keypressCallback');
+
+		// Fire the keypress event
+		events.keypress({ key: 'Enter' });
+	});
+});

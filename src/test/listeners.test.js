b5dc3c261436b1b16cfed12fc5167399de84aa6d
Miguel Ricaño <mricanho@gmail.com>
2021-04-19 12:18:40 -0500

    Add puppeteer

--- src/test/listeners.test.js
+++ src/test/listeners.test.js
@@ -1,4 +1,15 @@
+const puppeteer = require('puppeteer');
+
 it('should work', () => {
   const thisIsTrue = true;
   expect(thisIsTrue).toBe(true);
 });
+
+test('should create a new task', async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://example.com');
+  await page.screenshot({ path: 'example.png' });
+
+  await browser.close();
+})();
